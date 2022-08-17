f5bab39f81f5ee3033ec9d9b3b36c503f6faa673
Sergei Medvedev <clip1988@yandex.ru>
2022-08-17 21:24:32 +0300

    Objectiv 25 project

---
+++ Objectiv25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+for (int cof = number; cof <= number; cof++)
+{
+    Console.WriteLine($"Число А в натуральной степени числа В: {Math.Pow(cof,number1)}");
+}
