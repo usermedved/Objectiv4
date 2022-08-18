c83c51f491c876c1847d10c76e6d7e0ef039282b
Sergei Medvedev <clip1988@yandex.ru>
2022-08-18 20:19:36 +0300

    Objectiv 27 project

---
+++ Objectiv27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumber = 0;
+while(number != 0)
+{
+  sumNumber = sumNumber + number % 10;
+  number = number / 10;
+}
+Console.WriteLine($"Сумма числа: {sumNumber}");
