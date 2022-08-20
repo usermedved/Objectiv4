451734553036f6e73c0b1dac68e3646e630b60ce
Sergei Medvedev <clip1988@yandex.ru>
2022-08-20 16:43:24 +0300

    Objectiv 29 project

---
+++ Objectiv29/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] array = new int[8];
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = new Random().Next(-10, 10);  
+}
+Console.Write($"Вывод массива: [{String.Join(", ", array)}]");
